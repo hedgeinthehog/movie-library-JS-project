b8edb385775bf8a37828e9b30f55a82626e22adf
Igor <70631855+Igor-Tomilo@users.noreply.github.com>
2021-03-31 19:40:11 +0300

    .

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,11 +1,10 @@
 // jQuery(document).redy(function () {
 //   let degress = 0;
-//     timer = setInterval(function () {
-//         jQuery('team-wrapper').css('background-image', 'linear-gradient(
-//     0deg,
-//     rgba(180, 58, 151, 1) 0%,
-//     rgba(29, 195, 253, 1) 50%,
-//     rgba(159, 252, 69, 1) 77%
-//   );'
+//   timer = setInterval(function () {
+//     degress++;
+//     jQuery('team-wrapper').css(
+//       'background-image',
+//       'linear-gradient(' + degress + ' deg, #cfecd0, #a0cea7, #9ec0db',
+//     );
 //   }, 60000 / 360);
 // });
