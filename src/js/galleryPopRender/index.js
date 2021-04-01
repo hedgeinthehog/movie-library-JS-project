2f3c605610e59e493055c617f3d979f1bb11a5c9
Maksym <maxthebest1906@gmail.com>
2021-04-01 14:03:17 +0300

    input search is ready

--- src/js/galleryPopRender/index.js
+++ src/js/galleryPopRender/index.js
@@ -1,10 +1,28 @@
 import MoviePagination from '../pagination/moviePagination';
+import refs from '../refs'
+import debounce from 'lodash.debounce';
 
-const prevRef = document.querySelector('.prev');
-const nextRef = document.querySelector('.next');
+const {prevRef, nextRef, searchInpRef} = refs;
 
-const movie = new MoviePagination('.movies-list');
+const movie = new MoviePagination('.movies-list', '#search-input');
 movie.init();
+
+searchInpRef.addEventListener('input', debounce(requestMovie, 1000));
 
 prevRef.addEventListener('click', movie.goToPrevPage);
 nextRef.addEventListener('click', movie.goToNextPage);
+
+function requestMovie(){            //searching logic movies by query or popular
+    if(!searchInpRef.value) {
+        movie.pageReset();
+        movie.searchKey = '';
+        movie.byQueryFlag = false;
+        return movie.init();
+    }
+    if(searchInpRef.value){
+        movie.pageReset();
+        movie.searchKey = searchInpRef.value;
+        movie.byQueryFlag = true;
+        return movie.init();
+    }
+}
